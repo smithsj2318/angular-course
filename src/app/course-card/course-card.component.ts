6aae97cfc0d34782e554cb7c15210a9e2559673c
Steven J. Smith <smithsj2318@icloud.com>
2023-10-04 21:38:11 -0400

    Changed Input to be required and an object.

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {Course} from '../model/course';
 
 @Component({
   selector: 'course-card',
@@ -7,6 +8,6 @@ import {Component, Input} from '@angular/core';
 })
 export class CourseCardComponent {
 
-  @Input()
-  title: string;
+  @Input({required: true})
+  course: Course;
 }

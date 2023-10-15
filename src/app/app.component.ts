25b101e9a2ee15b06c026ebc6121254a8d91529b
Steven J. Smith <smithsj2318@icloud.com>
2023-10-15 16:18:35 -0400

    Covered Built in Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,16 @@ import {Course} from './model/course';
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  title = COURSES[2].description;
+
+  price = 9.99;
+
   coreCourse = COURSES[0];
   rxjsCourse = COURSES[1];
   ngrxCourse = COURSES[2];
+
 
   onCourseSelected = (course: Course) => {
     console.log('Card clicked', course);
